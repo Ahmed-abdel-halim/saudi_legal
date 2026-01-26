514bd902f2c35679ca59d1b6834be69448484ba3
Muhamed Elsayed <mohamedelsayed100100100@gmail.com>
2026-01-26 23:23:10 +0200

    Edit Invitation of Employement

---
+++ app/Mail/InviteEmployee.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class InviteEmployee extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $activationUrl;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(User $user, $activationUrl)
+    {
+        $this->user = $user;
+        $this->activationUrl = $activationUrl;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Invitation to Join Radiif Team',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.invite-employee',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
