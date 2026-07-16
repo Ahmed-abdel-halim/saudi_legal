d4bca6361645c4e7466abd7c14beadfb28aab7aa
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2026-07-16 14:36:58 +0300

    style(legal-assistant): set browser tab favicon to official Radiif logo

--- resources/views/saudi_legal/chat.blade.php
+++ resources/views/saudi_legal/chat.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>المساعد القانوني الذكي | رديف</title>
     
+    {{-- Favicons --}}
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/icon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('images/icon.png') }}">
     {{-- External CSS/JS Libraries --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;800;900&display=swap" rel="stylesheet">
